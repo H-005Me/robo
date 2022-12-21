d00b4a3e9d9ec5d90de3cca2152ca575dd46767e
H-005Me <h005itsme@gmail.com>
2022-12-21 11:20:49 +0200

    Cannot fully link zxing - package com.google.zxing.client.j2se does not exist.

---
+++ qr.java
@@ -0,0 +1,64 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.imageio.ImageIO;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatReader;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.NotFoundException;
+import com.google.zxing.Result;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.common.HybridBinarizer;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+
+public class qr
+{
+    /**
+     * 
+     * @param args 
+     * @throws WriterException
+     * @throws IOException
+     * @throws NotFoundException
+     */
+    public static void main(String[] args) throws WriterException, IOException, NotFoundException
+    {
+        String qrCodeData = "test";
+        String filePath = "temp.img";
+        String charset = "UTF-8"; // or "ISO-8859-1"
+        Map<EncodeHintType, ErrorCorrectionLevel> hintMap = new HashMap<EncodeHintType, ErrorCorrectionLevel>();
+        hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
+
+        System.out.println("Data read from QR Code: " + readQRCode(filePath, charset, hintMap));
+    }
+
+    /**
+     * 
+     * @param filePath
+     * @param charset
+     * @param hintMap
+     * 
+     * @return Qr Code value 
+     * 
+     * @throws FileNotFoundException
+     * @throws IOException
+     * @throws NotFoundException
+     */
+    public static String readQRCode(String filePath, String charset, Map hintMap)
+            throws FileNotFoundException, IOException, NotFoundException
+    {
+        BinaryBitmap binaryBitmap = new BinaryBitmap(new HybridBinarizer(
+                new BufferedImageLuminanceSource(ImageIO.read(new FileInputStream(filePath)))));
+        Result qrCodeResult = new MultiFormatReader().decode(binaryBitmap, hintMap);
+        return qrCodeResult.getText();
+    }
+}
